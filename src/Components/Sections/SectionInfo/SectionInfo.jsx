2183fdc57359a1ec7dd77bf4fdb0b735f1a70ae6
abdullohsoatov0001 <maxidaxakimova@gmail.com>
2025-11-21 11:58:41 +0300

    done

---
+++ src/Components/Sections/SectionInfo/SectionInfo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { main } from './SectionInfo.js';
+import img7 from './../../../assets/Frame 2031.png';
+
+const SectionInfo = () => {
+    const [openIndex, setOpenIndex] = useState(null);
+    const toggle = (i) => {
+        setOpenIndex(openIndex === i ? null : i);
+    };
+    return (
+        <>
+            {
+                main.map((item, index) => (
+                    <div className='max-w-[1300px] mx-auto px-5'>
+                        <div key={index} onClick={() => toggle(index)} className=' border-b border-[#D9D9D9]  py-[15px] flex flex-col gap-[10px] cursor-pointer'>
+                            <div className='flex items-center gap-5 justify-between'>
+                                <div className='font-bold'>
+                                    {item.title}
+                                </div>
+                                <img onClick={toggle} src={img7} className="text-xl transition-transform"
+                                    style={{
+                                        transform: openIndex === index ? "rotate(180deg)" : "rotate(0deg)",
+                                    }}>
+                                </img>
+                            </div>
+                            {
+                                openIndex === index && (
+                                    <div className='pl-5'>
+                                        {item.info}
+                                    </div>
+                                )
+                            }
+                        </div >
+                    </div>
+
+
+                ))
+            }
+        </>
+    )
+}
+
+export default SectionInfo
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
-import Section from '../Components/Sections/Section'
-
+import Section from '../Components/Sections/Section/Section'
+import SectionEmail from '../Components/Sections/SectionEmail/SectionEmail'
+import Sectioninfo from './../Components/Sections/SectionInfo/SectionInfo.jsx';
 const Home = () => {
     return (
         <div>
             <Header />
             <Section />
+            {/* <SectionEmail/> */}
+            <Sectioninfo/>
         </div>
     )
 }
