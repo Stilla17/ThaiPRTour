ed3c76874c2eb6786b734223f94c0c07ae32b1ad
abdullohsoatov0001 <maxidaxakimova@gmail.com>
2025-11-21 09:45:13 +0300

    Nav tayyor

---
+++ src/Components/Nav/NodeService.js
@@ -0,0 +1,88 @@
+import img1 from './../../assets/SettingsIcons.png';
+import img2 from './../../assets/SettingsIcons (1).png';
+import img3 from './../../assets/SettingsIcons (4).png';
+import img4 from './../../assets/Car.png';
+import img5 from './../../assets/SettingsIcons (2).png';
+import img6 from './../../assets/SettingsIcons (3).png';
+export  const main = [
+    {
+        title: 'Гражданство',
+        img: img1,
+        allLiks: [
+            {
+                links: ['Купить гражданство', 'Программы гражданства', 'Инвестиции', 'Юридическая помощь', 'Сотрудничество'],
+
+            },
+            {
+                link: ['Buy/citizenship', 'Citizenship/programs', 'Investments', 'Legal/assistance', 'Cooperation']
+
+            }
+        ]
+    },
+    {
+        title: 'Недвижимость',
+        img: img2,
+        allLiks: [
+            {
+                links: ['Купить', 'Снять', 'Коммерческая', 'Юридическая помощь', 'Предложить объект'],
+
+            },
+            {
+                link: ['Buy', 'Rent', 'Commercial', 'Legal/assistance', 'Offer/an/object']
+
+            }
+        ]
+    },
+    {
+        title: 'Индекс паспорта',
+        img: img3,
+        allLiks: [
+            {
+                links: ['Рейтинг паспортов', 'Безвизовые страны', 'Виза', 'Сравнение'],
+            },
+            {
+                link: ['Passport/ranking', 'Visa-free/countries', 'Visa', 'Comparison']
+
+            }
+        ]
+    },
+    {
+        title: 'Авто',
+        img: img4,
+        allLiks: [
+            {
+                links: ['Купить / продать', 'Купить / продать', 'Аренда', 'Сотрудничество'],
+            },
+            {
+                link: ['Buy/sell', 'Buy/sell', 'Rent', 'Cooperation']
+
+            }
+        ]
+    },
+    {
+        title: 'Консъерж - сервис',
+        img: img5,
+        allLiks: [
+            {
+                links: ['Услуги', 'Туризм', 'Транспорт', 'События', 'Подарки'],
+            },
+            {
+                link: ['Services', 'Tourism', 'Transport', 'Events', 'Gifts']
+
+            }
+        ]
+    },
+    {
+        title: 'Медиа',
+        img: img6,
+        allLiks: [
+            {
+                links: ['Блог', 'Новости', 'Видео', 'Подкасты'],
+            },
+            {
+                link: ['Blog', 'News', 'Videos', 'Podcasts']
+
+            }
+        ]
+    }
+];
