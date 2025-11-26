7f2df69241d95236aaf27fc73f0a1d5c8e5cd839
abdullohsoatov0001 <maxidaxakimova@gmail.com>
2025-11-26 12:39:35 +0300

    dhdsohf

--- src/Pages/Nedvij.jsx
+++ src/Pages/Nedvij.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
 import img from './../assets/Rectangle 1262.png';
 import "swiper/css";
 
 import SectionNedvij from '../Components/Sections/NedvigSections/SectionNedvij';
+import axios from 'axios';
 
 const Nedvij = () => {
+    const [data, setData] = useState([])
+    const handleGetData = async () => {
+        try {
+            const res = await axios.get('https://68e211a48943bf6bb3c58500.mockapi.io/todoList/objects');
+            setData(res.data)
+        } catch (element) {
+            console.error('Error:', element)
+        }
+    }
+
+    useEffect(() => {
+        handleGetData()
+    }, [])
     return (
         <>
             <section>
@@ -31,8 +45,21 @@ const Nedvij = () => {
                     <p className='text-[22px] font-bold'>Топ новостроек</p>
                     <p className='text-[#757575]'>Смотреть все</p>
                 </div>
-                
+
             </section>
+            {
+                data
+                    .filter(item => item.newbuild === "Новостройка")
+                    .map((item, index) => (
+                        <div key={index} className='border p-5 rounded-lg mt-5'>
+                            <img src={item.image} alt="" />
+                            <p className='text-[20px] font-bold mt-5'>{item.name}</p>
+                            <p className='text-[#757575]'>{item.address}</p>
+                            <p>{item.size}</p>
+                            <p className='text-[#202020] mt-2'>{item.info}</p>
+                        </div>
+                    ))
+            }
         </>
     )
 
